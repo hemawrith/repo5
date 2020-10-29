10a26e358fb379b863d6bc82146661511441c437
Alexandra Andreiciuc <aleeexabm@gmail.com>
2020-10-29 02:19:35 +0200

    Updating Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     //hello there
+    public static void myMethod1() {
+        System.out.println("Branch1 method");
+    }
+
+    public static void myMethod2() {
+        System.out.println("Nice job");
+    }
     public static void main(String[] args) {
 
     }
