672952479d1353b286207531a282d440b9f87641
Alexandra Andreiciuc <aleeexabm@gmail.com>
2020-10-29 02:24:11 +0200

    Updating Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     //hello there
+    public static void branchChanger() {
+        System.out.println("Brunch2 was changed again");
+    }
+    public static void opinion() {
+        System.out.println("It's pretty interesting");
+    }
     public static void main(String[] args) {
 
     }
